7846d2101c7b0be6ccadf7352563c7d22333d2d9
clementgaston31 <44499395+clementgaston31@users.noreply.github.com>
2018-11-13 15:38:11 +0100

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,11 +1,13 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
 #include "generationCode.h"
-int param = 0;
+
+/*int param = 0;
 int VPIN = 0;
 int SPIN = 0;
-int choixMenu = 1;
-int menuCoeur();
-int menuPoul();
+int choixMenu = 1;*/
+int menuCoeur(int choixMenu,int *param,int *VPIN,int *SPIN);
+int menuPoul(int choixMenu,int *param,int *VPIN,int *SPIN);
 void Color(int couleurDuTexte, int couleurDeFond);
+
 #endif // MENU_H_INCLUDED
