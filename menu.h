c51d86fabc1473f142520cbffa716f7c2376372f
EclipsioZ <44499419+EclipsioZ@users.noreply.github.com>
2018-11-13 14:52:25 +0100

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,6 +1,11 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
 #include "generationCode.h"
-
-
+int param = 0;
+int VPIN = 0;
+int SPIN = 0;
+int choixMenu = 1;
+int menuCoeur();
+int menuPoul();
+void Color(int couleurDuTexte, int couleurDeFond);
 #endif // MENU_H_INCLUDED
